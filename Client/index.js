6506ad86eed483b69ce7707eeb6fd756ec3e6654
yassine <yassine.benlamkadem@gmail.com>
2020-07-27 17:20:23 +0100

    Quick debugging

--- Client/index.js
+++ Client/index.js
@@ -1,10 +1,62 @@
-document.addEventListener("DOMContentLoaded", e => {
+//document.addEventListener("DOMContentLoaded", e => {
     const url = "http://localhost:3000/posts"
     // Grab relevant HTML elements
     const postContainer = document.getElementById("postContainer");
 
     // Insert previous posts into postContainer
+    let article = document.createElement("article");
     
+    // Start Add gif functionality //
+    const GIPHYForm = document.getElementById('GIPHYSearchForm')
+    const GIPHY_KEY = 'BZM019G18oCCblfou5NgheomVPbrjKsK';
+    
+    // Displays search bar/button for GIPHY when clicked
+    function showGIPHY() {
+        let x = document.querySelector(".GIPHYSearchInputContainer");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    }
+    
+    // Calls a fetch on GIPHY API and returns 6 images into div called "thumbs"
+    function giphySearch(keyword) {
+        return fetch(`http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${GIPHY_KEY}&limit=6`)
+        .then(response => response.json())
+        .then(jsonData => gotData(jsonData))
+    }
+    
+    // Appends clicked GIF to journal entry form
+    function GIFClick(img){
+         document.querySelector('#userInput').appendChild(img);
+         //article.appendChild(img);
+    }
+    
+    // appends the six GIFs returned from search into a div to be selected
+    function appendImage(giphy) { 
+        const img = document.createElement('img'); 
+        img.src = `${giphy}`; 
+        img.className = "GIPHYResult";
+        //img.id = "selectedGIF";
+        img.setAttribute('onclick','GIFClick(this)');
+        document.getElementById('thumbs').appendChild(img);
+    }  
+    
+    // Chained function with appendImage
+    function gotData(data) {
+        for (let i = 0; i < data.data.length; i++) {
+        appendImage(data.data[i].images.original.url)
+        }
+    }
+    
+    // Event listener for searching for GIFs
+    GIPHYForm.addEventListener("click", e => {
+        e.preventDefault()
+        document.querySelectorAll('.GIPHYResult').forEach(e => e.remove());
+        const searchTerm = document.getElementById('GIPHYsearchInput').value;
+        giphySearch(searchTerm)  
+    })
     
     // Upon submit, display user input on current page
     // How do you do this with a callback instead of an arrow function?
@@ -17,7 +69,6 @@ document.addEventListener("DOMContentLoaded", e => {
         let postBody = document.getElementById("postBody").value;
 
         // Update page with new post - create article, h2, and p tags for new post
-        let article = document.createElement("article");
         let articleHead = document.createElement("h2");
         let articleBody = document.createElement("p");
 
@@ -54,12 +105,20 @@ document.addEventListener("DOMContentLoaded", e => {
         .catch(console.warn);
     });
 
+    // const discard = document.getElementById("Discard")
+    // document.addEventListener("click", e => {
+    //     e.preventDefault();
 
-})
+    //     let postTitle = document.getElementById("postTitle").value;
+    //     let postBody = document.getElementById("postBody").value;
+        
+    //     postTitle = "";
+    //     postBody = "";
+    // })
+
+//})
 
 
 // Add emoji functonality
-
-// Add gif functionality
 
 // Discard button resets title and body
